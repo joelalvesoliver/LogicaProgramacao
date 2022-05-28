c0483b1261bdf15c036bf45a4ec17f8723820d02
joelalvesoliver <joel.alves.oliver@gmail.com>
2022-05-27 21:07:43 -0300

    Add project files.

---
+++ Arquivos/Program.cs
@@ -0,0 +1,89 @@
+﻿// See https://aka.ms/new-console-template for more information
+#region AllLines
+var fileA = File.ReadAllLines("names1.csv");
+var fileB = File.ReadAllLines("names2.csv");
+
+foreach (var n in fileA)
+{
+    Console.WriteLine($"ArquivoA: {n}");
+}
+
+foreach (var n in fileB)
+{
+    Console.WriteLine($"ArquivoB: {n}");
+}
+
+var merge = fileA.Union(fileB);
+
+foreach (var n in merge)
+{
+    Console.WriteLine($"Merge: {n}");
+}
+
+var groupBy = from name in merge
+              let n = name.Split(',')
+              group name by n[0][0] into g
+              orderby g.Key
+              select g;
+
+foreach (var g in groupBy)
+{
+    string fileName = @"testFile_" + g.Key + ".txt";
+
+    Console.WriteLine(g.Key);
+
+    File.WriteAllLines(fileName, g);
+}
+#endregion
+
+#region LineByLine
+List<string> fileC = new();
+using (StreamReader sR = new StreamReader("names1.csv"))
+{
+    string linha = null;
+    while ((linha = sR.ReadLine()) != null)
+    {
+        fileC.Add(linha);
+    }
+}
+
+var fileD = File.ReadAllLines("names2.csv");
+foreach (var n in fileA)
+{
+    Console.WriteLine($"ArquivoA: {n}");
+}
+
+foreach (var n in fileB)
+{
+    Console.WriteLine($"ArquivoB: {n}");
+}
+
+var mergeA = fileC.Union(fileD);
+
+foreach (var n in merge)
+{
+    Console.WriteLine($"Merge: {n}");
+}
+
+var groupByA = from name in mergeA
+              let n = name.Split(',')
+              group name by n[0][0] into g
+              orderby g.Key
+              select g;
+
+foreach (var g in groupByA)
+{
+    string fileName = @"testFile_" + g.Key + ".txt";
+
+    Console.WriteLine(g.Key);
+
+    using (StreamWriter sw = new StreamWriter(fileName))
+    {
+        foreach (var item in g)
+        {
+            sw.WriteLine(item);
+            Console.WriteLine(item);
+        }
+    }
+}
+#endregion
---
+++ LogicaProgramacao/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+
+
+var A = new string[] { "as", "b" };
+
+var x = true;
+var y = true;
+Console.WriteLine("XOR (Exclusive OR) operator exmample");
+Console.WriteLine("*******************************");
+//Console.WriteLine("Texto sem ascento - {0}", RemoverAcentuacao("texto com ascentuaÇão"));
+
+Dictionary<char, int> _romanMap = new Dictionary<char, int>
+{
+   {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
+};
+
+Console.WriteLine(ConvertRomanToNumber("CMCM").ToString());        
+
+
+string RemoverAcentuacao(string text)
+{
+    Console.WriteLine(text.Normalize(NormalizationForm.FormD)); 
+    return new string(text
+        .Normalize(NormalizationForm.FormD)
+        .Where(ch => char.GetUnicodeCategory(ch) != UnicodeCategory.NonSpacingMark)
+        .ToArray());
+}
+
+
+
+int ConvertRomanToNumber(string text)
+{
+    int totalValue = 0, prevValue = 0;
+    foreach (var c in text)
+    {
+        if (!_romanMap.ContainsKey(c))
+            return 0;
+        var charValue = _romanMap[c];
+        totalValue += charValue;
+        if (prevValue != 0 && prevValue < charValue)
+        {
+            if (prevValue == 1 && (charValue == 5 || charValue == 10)
+                || prevValue == 10 && (charValue == 50 || charValue == 100)
+                || prevValue == 100 && (charValue == 500 || charValue == 1000))
+                totalValue -= 2 * prevValue;
+            else
+                return 0;
+        }
+        prevValue = charValue;
+    }
+    return totalValue;
+}
